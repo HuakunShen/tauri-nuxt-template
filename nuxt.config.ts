2289c7088ea500fe6fdb956a2302b0a19b18df97
Huakun Shen <huakun.shen@huakunshen.com>
2023-04-06 03:53:46 -0400

    Minimal working example. API verified to work with nuxt, but removed from template to keep it simple.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: true // use nuxt generate to generate SSG, if ssr is false, single page app will be generated
+    ssr: false // SSR must be turned off
 })
